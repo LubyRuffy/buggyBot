3c4cbbb957e394404431df674dc6fbc4622d05e4
dandrews <danielandrews7396@gmail.com>
2017-07-25 01:57:21 +0100

    setting up buggy class

---
+++ buggy.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3.4
+
+#####################################################
+##              This is the buggyBot               ##
+##   the first robotics Project from dandrews7396  ##
+#####################################################
+
+import time
+import numpy as np
+import explorerhat as eh
+import cv2
+import scipy
+from picamera import PiCamera
+import
+
+## Set-up a class that forms the basis for our buggyBot this will allow us to update
+## the bot as we go.
+class buggBot(object):
+    """A buggy that's going to roam over the house. A lot of these will be None."""
+
+    def __init__(self):
+        self.time = None # Start the navigation time
+        self.total_time = None # Total time passed
+        self.img = None # No image to begin with
+        self.yaw = None # Not yet initialised
+        self.pitch = None # Not yet initialised
+        self.roll = None # Not yet initialised
+        self.vel = None # Not yet initialised
+        self.motor1 = eh.motor.one
+        self.motor2 = eh.motor.two
+        self.max_vel = 100
+        self.buggy_vision = np.zeros((160,230,3), dtype = np.float)
+        self.world_map = np.zeros((200, 200, 3), dtype = np.float)
