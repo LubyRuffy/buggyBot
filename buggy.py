1aafeaf420d00a495350da56e36a8bcb573392a0
dandrews <danielandrews7396@gmail.com>
2017-08-10 01:23:45 +0100

    Feat:   Added methods for calculating Euler angles and capturing Images
    `_calc_pitch()` and `_calc_roll()` read in data from mpu6050 over i2c
    and `_take_picture()` takes an image as an rgb array and then takes a
    standard image and saves it in the `/images` folder.

--- buggy.py
+++ buggy.py
@@ -18,7 +18,7 @@ import matplotlib.image as mpimg
 from mpu6050 import mpu6050
 ## Set-up a class that forms the basis for our buggyBot this will allow us to update
 ## the bot as we go.
-class buggBot(object):
+class buggyBot(object):
     """A buggy that's going to roam over the house. A lot of these will be None."""
 
     def __init__(self):
@@ -26,6 +26,7 @@ class buggBot(object):
         self.time = None # Start the navigation time
         self.total_time = None # Total time passed
         self.img = None # No image to begin with
+        self.img_array = None # Image will also be saved as np array, so no need to convert.
         self.yaw = None # Not yet initialised
         self.pitch = None # Not yet initialised
         self.roll = None # Not yet initialised
@@ -34,13 +35,15 @@ class buggBot(object):
         self.motor1 = eh.motor.one
         self.motor2 = eh.motor.two
         self.max_vel = 100
+        ## Add in address using sudo i2cdetect -y 1
         self.mpu_sensor = mpu6050(## address goes here)
         self.x_acc_ctr = None   ## These will need
         self.y_acc_ctr = None   ## Populating on flat, level
         self.z_acc_ctr = None   ## When stationary.
+        self.camera = Picamera()
         self.buggy_vision = np.zeros((160,230,3), dtype = np.float)
         self.world_map = np.zeros((200, 200, 3), dtype = np.float)
-
+        self._img_file = './images/'
 
 
     def update(self):
@@ -48,10 +51,14 @@ class buggBot(object):
         if self.start_time == None:
             self.start_time = time.time()
             self.total_time = 0
+
             if self.mpu_sensor.read_accel_range not 2:
                 self.mpu_sensor.set_aacel_range(ACCEL_RANGE_2G)
             if self.mpu_sensor.read_gyro_range not 250:
                 self.mpu_sensor.set_gyro_range(GYRO_RANGE_250DEG)
+            self.camera.resolution = (320, 240)
+            self.camera.framerate = 24
+            self.camera.rotation = 180
         else:
             tot_time = time.time() - self.start_time
             if np.isinfinite(tot_time):
@@ -62,3 +69,30 @@ class buggBot(object):
             mpu_temp = mpu_data[0]
             mpu_accel = mpu_data[1]
             mpu_gyro = mpu_data[2]
+            self.pitch = self._calc_pitch(mpu_accel)
+            self.roll = self._calc_roll(mpu_accel)
+            self.img, self.img_array = self._take_picture()
+
+    def _calc_pitch(self, accel_data):
+        """ Calculate pitch angle of buggyBot."""
+        x = accel_data["x"]
+        y = accel_data["y"]
+        z = accel_data["z"]
+        denom = np.sqrt(y**2 + z**2)
+        return arctan2(x, denom)
+
+    def _calc_roll(self, accel_data):
+        """ Calculate roll angle of buggyBot."""
+        x = accel_data["x"]
+        y = accel_data["y"]
+        z = accel_data["z"]
+        denom = np.sqrt(x**2 + z**2)
+        return arctan2(y, denom)
+
+    def _take_picture():
+        """ Take picture as both standard image an np array."""
+        timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+        array_out = np.empty((240, 320, 3), dtype=np.uint8)
+        self.camera.capture(array_out, 'rgb')
+        std_img = self.camera.capture(self._img_file + '{}.jpg'.format(timestamp))
+        return std_img, array_out
